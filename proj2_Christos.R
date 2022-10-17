c3ddcaa4bbf6bc31252acea2a4e6e276d9ffe7da
ChristosGiannikos98 <113847135+ChristosGiannikos98@users.noreply.github.com>
2022-10-17 15:13:22 +0100

    Add files via upload
    I think pone and pall are complete. I've also tried dloop, which needs correcting and improvement.

--- proj2_Christos.R
+++ proj2_Christos.R
@@ -1,23 +1,28 @@
-simulation_experiment <- function (n,b,k,strategy){
+simulation_experiment <- function (b,k,strategy){
     box_values <- b
-    choice <- 1:n # Generates the choice of boxes the prisoner will take depending on the strategy
+    n=length(b)/2
+    choice <- c() # Generates the choice of boxes the prisoner will take depending on the strategy
     if (strategy == 1){
       choice[1] <- box_values[k]
-      for (i in 2:n){
-        choice[i] <- box_values[choice[i-1]]
+      i=1
+      while (choice[i]!=k && i<=n){
+        choice[i+1] <- box_values[choice[i]]
+        i=i+1
       }
     }
     if (strategy == 2){
       random <- sample(1:(2*n), 1, replace = FALSE)
       choice[1] <- box_values[random]
-      for (i in 2:n){
-        choice[i] <- box_values[choice[i-1]]
+      i=1
+      while ((choice[i]!=k) && (i<=n)){
+        choice[i+1] <- box_values[choice[i]]
+        i=i+1
       }
     }
     if (strategy == 3){
-      choice <- sample(1:(2*n),n, replace = FALSE)
+      choice <-box_values[sample(1:(2*n),n, replace = FALSE)]
     }
-    return (choice)
+    return (k %in% choice)
 }
 
 
@@ -25,7 +30,7 @@ pone <- function(n, k, strategy, nreps){
   count = 0 # initialise counter
   for (i in 1:nreps){
     b <- sample(1:(2*n), 2 * n, replace = FALSE)
-    if (k %in% simulation_experiment(n,b,k,strategy)){
+    if (simulation_experiment(b,k,strategy)==TRUE){
       count=count+1
     }
   }
@@ -35,21 +40,19 @@ pone <- function(n, k, strategy, nreps){
 
   
 pall <-  function(n, strategy, nreps){
-  count_all=0
+  count=0
   for (i in 1:nreps){
     b <- sample(1:(2*n), 2 * n, replace = FALSE)
-    count=0
-    for (k in 1:(2*n)){
-      if (k %in% simulation_experiment(n,b,k,strategy)){
-        count=count+1
-      }
+    k=1
+    while (simulation_experiment(b,k,strategy)==TRUE && k<2*n){
+      k=k+1
     }
-    if (count==2*n){
-      count_all=count_all+1
+    if (k==2*n){
+      count=count+1
     }
   }
-  count_all=count_all/nreps
-  return (count_all)
+  count=count/nreps
+  return (count)
 }  
 
 
@@ -81,7 +84,9 @@ dloops <- function(n,nreps){
       }
     }
   }
-  #p=p/nloops
+  p=p/nloops
   return (p)
   cat(p)
 }
+
+
