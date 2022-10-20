8b2226078a80d83040d6fb4af0305e50980415ae
Patrick Renaud <P.M.Renaud@sms.ed.ac.uk>
2022-10-20 16:21:28 +0100

    dloop again

--- proj2_Patrick.R
+++ proj2_Patrick.R
@@ -54,34 +54,6 @@ dloops <- function(n,nreps){
 
 
 
-dloops <- function(n,nreps){
-  p=array(0,2*n)
-  for (i in 1:nreps){
-    s=array(NA,2*n)
-    b <- sample(1:(2*n), 2*n, replace = FALSE)
-    s[1] <- b[1]
-    if (s[1]==1){
-      p[sum(!is.na(s))]<-p[sum(!is.na(s))]+1
-    }else{
-      for (i in 2:(2*n)){
-        s[i] <- b[s[i-1]]
-        
-        if(s[i]==b[1]){
-          p[sum(!is.na(s))]<-p[sum(!is.na(s))]+1
-          break
-        }
-      }
-    }
-  }
-  print(p)
-  sum(p)
-}
-
-
-
-
-
-
 
 
 
@@ -104,8 +76,54 @@ dloops <- function(n,nreps){
       p[sum(!is.na(s))]<-p[sum(!is.na(s))]+1
     }
   }
+  print(p)
   p=p/sum(p)
   print(p)
 }
 
-  
+
+
+
+
+
+
+
+
+check <- function(k, a){
+  ## Function checks if a number k is in a vector a if so it returns 1 else 0.
+  if (k %in% a){return(1)}
+  else{return(0)}
+}
+
+
+
+
+dloops <- function(n,nreps){
+  p=array(0,2*n)
+  for (h in 1:nreps){
+    s=array(NA,c(2*n,2*n))
+    b <- sample(1:(2*n), 2*n, replace = FALSE)
+    for (j in b)
+      if (check(j,s[j,])==0){
+        s[j,1] <- b[j]
+        for (i in 2:(2*n)){
+          s[j,i] <- b[s[j,i-1]]
+          if(s[j,i]==b[j]){
+            p[sum(!is.na(s[j,]))-1]<-p[sum(!is.na(s[j,]))-1]+1
+            break
+          }
+        }
+        if (s[j,i]!=b[j]){
+          p[length(s[j,])]<-p[length(s[j,])]+1
+        }
+      }
+  }
+  print(p[1:100])
+  p_len<-p[1:100]/sum(p[1:100])
+
+  return(p_len)
+}
+p <- dloops(50,1000)
+p1 <- p[1:100]
+p1
+scatter.smooth(1:100,p1)
