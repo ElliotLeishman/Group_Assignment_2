9d76a9e410dbbe12d2994f89d256bef08db467bf
ChristosGiannikos98 <113847135+ChristosGiannikos98@users.noreply.github.com>
2022-10-20 22:15:19 +0100

    Add files via upload
    I think I have a better expression for dloops but the results are the same!

--- proj2_Christos.R
+++ proj2_Christos.R
@@ -57,7 +57,7 @@ pall <-  function(n, strategy, nreps){
 
 
 
-dloops <- function(n,nreps){
+dloopsold  <- function(n,nreps){
   p=rep(0,2*n)
   nloops=0
   for (i in 1:nreps){
@@ -88,5 +88,45 @@ dloops <- function(n,nreps){
   return (p)
   cat(p)
 }
-b <-sample(1:(200), 200, replace = FALSE)
-b
+
+
+
+check <- function(k, a){
+  ## Function checks if a number k is in a vector a if so it returns 1 else 0.
+  if (k %in% a){return(1)}
+  else{return(0)}
+}
+
+
+
+
+dloops <- function(n,nreps){
+  p=array(0,2*n)
+  for (h in 1:nreps){
+    s=array(0,c(2*n,2*n))
+    b <- sample(1:(2*n), 2*n, replace = FALSE)
+    for (j in b){
+      if (check(j,s)==0){
+        s[j,1] <- b[j]
+        k=1
+        while ((s[j,k]!=j) && (k<(2*n))) {
+          s[j,k+1]=b[s[j,k]]
+          k=k+1
+        }
+        p[k]=p[k]+1
+        }
+    }
+  }
+  p_len<-p[1:(2*n)]/sum(p[1:(2*n)])
+  
+  return(p_len)
+}
+
+
+p <- dloops(50,10000)
+p1 <- p[1:100]
+p1
+scatter.smooth(1:100,p1)
+1-sum(p1[51:100])
+
+
